21714e9f13a2572233a07551658ba06ef2e74fc8
Fredy Sudhir <39754501+fredysj@users.noreply.github.com>
2023-11-08 11:06:47 -0500

    assignment 3

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./assets/react-logo.svg";
+import {FaUserLarge} from "react-icons/fa6"
 
 // This creates an object which is then rendered in the root element.
-const parent = React.createElement(
-	"div",
-	{ id: "parent" },
-	React.createElement(
-		"div",
-		{id:'children'},
-		[React.createElement( 'h1', {}, "I'm a h1 tag"), React.createElement( 'h2', {}, "I'm a h2 tag")]
-	)
-)
-console.log(parent)
+// const parent = React.createElement(
+// 	"div",
+// 	{ class: "title" },
+// 	[React.createElement( 'h1', {}, "I'm a h1 tag"), React.createElement( 'h2', {}, "I'm a h2 tag"), React.createElement( 'h3', {}, "I'm a h3 tag")]
+// )
 
-const heading = React.createElement( 'h1', {id: 'heading', attribute:"abc"}, 'Hello world!!')
+// const jsxElem = (
+// 	<div className="title">
+// 		<h1>H1 title</h1>
+// 		<h2>I am a h2 tag</h2>
+// 		<h3>H3 heading</h3>
+// 	</div>
+// )
+
+// const TitleComponent = () => (
+// 	<div className="title">
+//  		<h1>H1 title</h1>
+// 		<h2>I am a h2 tag</h2>
+//  		<h3>H3 heading</h3>
+// 		<img src={logo} />
+//  	</div>
+// )
+
+const HeaderComponent = () => (
+	<div className="header">
+		<div className="wrapper header__wrap">
+			<img src={logo} alt="React" className="header__logo"/>
+			<input type="text" className="header__search" placeholder="Search..."/>
+			<FaUserLarge className="header__user-icon"/>
+		</div>
+	</div>
+)
+
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 
 // Renders the object created using createElement into the root.
-root.render(parent);
+root.render(<HeaderComponent/>);
