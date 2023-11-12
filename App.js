fe3768ce51f4a9e53855bf098fc76adc4d27902e
Fredy Sudhir <39754501+fredysj@users.noreply.github.com>
2023-11-11 22:03:07 -0500

    Assignment 4

--- App.js
+++ App.js
@@ -2,42 +2,70 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import logo from "./assets/react-logo.svg";
 import {FaUserLarge} from "react-icons/fa6"
+import data from './data.json'
 
-// This creates an object which is then rendered in the root element.
-// const parent = React.createElement(
-// 	"div",
-// 	{ class: "title" },
-// 	[React.createElement( 'h1', {}, "I'm a h1 tag"), React.createElement( 'h2', {}, "I'm a h2 tag"), React.createElement( 'h3', {}, "I'm a h3 tag")]
-// )
-
-// const jsxElem = (
-// 	<div className="title">
-// 		<h1>H1 title</h1>
-// 		<h2>I am a h2 tag</h2>
-// 		<h3>H3 heading</h3>
-// 	</div>
-// )
-
-// const TitleComponent = () => (
-// 	<div className="title">
-//  		<h1>H1 title</h1>
-// 		<h2>I am a h2 tag</h2>
-//  		<h3>H3 heading</h3>
-// 		<img src={logo} />
-//  	</div>
-// )
-
-const HeaderComponent = () => (
-	<div className="header">
-		<div className="wrapper header__wrap">
-			<img src={logo} alt="React" className="header__logo"/>
-			<input type="text" className="header__search" placeholder="Search..."/>
-			<FaUserLarge className="header__user-icon"/>
+const Header = () => {
+	return (
+		<div className="header">
+			<div className="logo-container">
+				<img src="https://i.pinimg.com/originals/34/0c/6a/340c6add7519212185a08d4205eb1965.png" className="logo"/>
+			</div>
+			<div className="nav-items">
+				<ul>
+					<li>Home</li>
+					<li>About Us</li>
+					<li>Contact Us</li>
+					<li>Cart</li>
+				</ul>
+			</div>
 		</div>
-	</div>
-)
+	)
+}
+
+const RestoCard = (props) => {
+	const { resData } = props
+	const { name, cloudinaryImageId, cuisines, avgRating, sla, costForTwo } = resData?.info
+	return (
+		<div className="res-card">
+			<img
+				className="res-card__img"
+				src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}
+			/>
+			<h3 className="res-card__name">{name}</h3>
+			<p className="res-card__cuisine">{cuisines.join(", ")}</p>
+			<div>
+				<span className="res-card__rating">{avgRating}</span>
+				<span className="res-card_cost">{costForTwo}</span>
+			</div>
+			
+			<p className="res-card__eta">{`${sla.deliveryTime} mins`}</p>
+		</div>
+	)
+}
+
+const Body = () => {
+	return (
+		<div className="body">
+			<div className="search">Search</div>
+			<div className="res-container">
+				{
+					data.map((restaurant) => (<RestoCard key={restaurant.info.id} resData={restaurant} />))
+				}
+			</div>
+		</div>
+	)
+}
+
+const AppLayout = () => {
+	return (
+		<div className="app">
+			<Header />
+			<Body />
+		</div>
+	)
+}
 
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 
 // Renders the object created using createElement into the root.
-root.render(<HeaderComponent/>);
+root.render(<AppLayout/>);
